65110788f3582a5f1815bfb28d6b586dcb9e8622
mikhailtarasevich <tarasevichmikhail93@gmail.com>
2023-06-22 20:00:34 +0300

    init commit

---
+++ src/main/java/com/tarasevich/mikhail/eventmanger/restapieventmanger/RestApiEventMangerApplication.java
@@ -0,0 +1,13 @@
+package com.tarasevich.mikhail.eventmanger.restapieventmanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiEventMangerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiEventMangerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tarasevich/mikhail/eventmanger/restapieventmanger/RestApiEventMangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarasevich.mikhail.eventmanger.restapieventmanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiEventMangerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
